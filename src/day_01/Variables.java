ca6810ab79ab0a602f05f9476f2502d86e620a69
Kutay <kutaykubilaykayhan@gmail.com>
2025-03-02 18:49:03 +0300

    Project and Variables Class is created

---
+++ src/day_01/Variables.java
@@ -0,0 +1,34 @@
+package day_01;
+
+public class Variables {
+
+    public static void main(String[] args) {
+
+        boolean bl1;
+        boolean bl2 = true;
+        bl1 = false;
+        int num1, num2, num3 = 5, num5;
+        double db1,db2;
+        char cr1 = 'A';
+        char cr2 = 65;
+
+        String str1 = "I love Java";
+
+        int myNum1 = 65;
+        int myNum2 = 'A';
+
+        System.out.println("myNum1 = " + myNum1);
+        System.out.println("myNum2 = " + myNum2);
+
+        System.out.println("cr1 = " + cr1);
+        System.out.println("cr2 = " + cr2);
+
+        System.out.println("bl2 = " + bl2);
+        System.out.println("bl1 = " + bl1);
+
+        System.out.println(str1);
+
+        System.out.println("Text Message");
+
+    }
+}
