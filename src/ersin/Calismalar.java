484b52f743ce8c3207fe66837899becccce5ff92
Ersin <michael.knight@cydeo.com>
2025-03-15 22:21:59 +0300

    Calismalar Class is updated.

--- src/ersin/Calismalar.java
+++ src/ersin/Calismalar.java
@@ -3,6 +3,8 @@ package ersin;
 public class Calismalar {
     public static void main(String[] args) {
 
+        String name = "Michael";
+        String surname = "Kayhan";
         int age = 21;
 
     }
